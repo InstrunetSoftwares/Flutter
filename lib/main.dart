e8cf50382550915387deb19fedcd24c7f9923f15
AXCWG <xiey0@qq.com>
2025-11-05 22:51:29 +0800

    need apis.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:audio_metadata_reader/audio_metadata_reader.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:window_size/window_size.dart';
 
 void main() {
@@ -33,7 +34,7 @@ class _MainAppState extends State<MainApp> {
             UploadPage(),
             Center(child: Text("Search")),
             Center(child: Text("Settings")),
-            Center(child: Text("Profile"),)
+            ProfilePage()
           ],
         ),
 
@@ -184,4 +185,15 @@ class _UploadPageState extends State<UploadPage> with AutomaticKeepAliveClientMi
   bool get wantKeepAlive => true;
 }
 
-
+class ProfilePage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _ProfilePageState();
+}
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
